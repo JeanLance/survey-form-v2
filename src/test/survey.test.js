5e59c8a039bc7d9224c99ce6e606f0a0863b65ca
Jean Lance <jean.lance.lumbad.carpio@gmail.com>
2022-04-08 13:42:17 +0800

    test

--- src/test/survey.test.js
+++ src/test/survey.test.js
@@ -8,6 +8,7 @@ const SurveyModel           = require('../models/survey.model');
 describe("Survey Model", function(){
 
     it('Should return success message when input captcha matches.', function(){
+        console.log("qwe");
         let surveyModel = new SurveyModel();
         let captcha = surveyModel.captcha;
         let result = surveyModel.verifyCaptchaInput(captcha);
